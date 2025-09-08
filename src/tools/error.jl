6a6be50bb91be7e08be6233bdd32907bde273273
Raul Durand <raul.durand@gmail.com>
2025-09-08 15:54:30 -0300

    Clean up error handling code by removing commented-out sections and unused exception structures

--- src/tools/error.jl
+++ src/tools/error.jl
@@ -9,10 +9,6 @@ end
 
 function Base.showerror(io::IO, e::StopException, bt; backtrace=false)
     printstyled(io, e.message, "\n", color=:red)
-    # showerror(io, e.message)
-    # Base.with_output_color(get(io, :color, false) ? :green : :nothing, io) do io
-    #     showerror(io, e.message)
-    # end
 end
 
 export stop
@@ -24,12 +20,6 @@ mutable struct SerendipException <: Exception
 end
 
 Base.showerror(io::IO, e::SerendipException) = printstyled(io, "SerendipException: ", e.message, "\n", color=:red)
-
-# mutable struct PropertyException <: Exception
-#     message::String
-# end
-
-# Base.showerror(io::IO, e::PropertyException) = print(io, "PropertyException: ", e.message)
 
 
 macro check(expr, args...)
@@ -49,45 +39,10 @@ macro check(expr, args...)
         error("Invalid number of arguments for @check macro. Expected 0, 1, or 2 arguments, got $(length(args))")
     end
 
-    # expr = replace(string(expr), " "=>"")
-
     return quote
         if !$(esc(expr)) # Eval boolean expression
-            # Get function name
-            # st = stacktrace(backtrace())
-            # fname = :_
-            # for frame in st
-            #     if !startswith(string(frame.func), "_") && frame.func!=Symbol("macro expansion")
-            #         fname = frame.func
-            #         break
-            #     end
-            # end
-
             msg = $(esc(msg))
-            # fname != "" && (msg="$fname: $msg")
             throw($(exception)(msg))
         end
     end
 end
-
-
-# macro checkmissing(params, required, names)
-#     return quote
-#         # Get function name
-#         st = stacktrace(backtrace())
-#         fname = :_
-#         for frame in st
-#             if !startswith(string(frame.func), "_") && frame.func!=Symbol("macro expansion")
-#                 fname = frame.func
-#                 break
-#             end
-#         end
-
-#         missingkeys = setdiff($(esc(required)), keys($(esc(params))) )
-#         if length(missingkeys)>0
-#             msg = "Missing arguments: $(join(missingkeys, ", ")). Possible inputs are: $($(esc(names)))"
-#             msg = replace(msg, "=" => ":")
-#             throw(SerendipException("$fname: $msg"))
-#         end
-#     end
-# end
