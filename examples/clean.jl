b9054f9646052cedeb21f7abaf85e178651a5afe
Raul Durand <raul.durand@gmail.com>
2025-12-12 18:52:33 -0300

    cleaning updated

--- examples/clean.jl
+++ examples/clean.jl
@@ -6,7 +6,7 @@ println("  deleting temporary files...")
 for (root, dirs, files) in walkdir(path)
     for file in files
         ext = split(file*".", ".")[2]
-        if contains(ext, r"vtk|vtu|pdf|dat|table|book|log")
+        if contains(ext, r"step|vtk|vtu|pdf|dat|table|book|log")
             fullname = joinpath(root, file)
             rm(fullname)
         end
