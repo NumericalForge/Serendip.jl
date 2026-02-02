acc4b93a94e13e3c3c8f69595c79f99c5ca14e80
Raul Durand <raul.durand@gmail.com>
2026-02-02 09:47:29 -0300

    fix: correct formatting

--- src/tools/constants.jl
+++ src/tools/constants.jl
@@ -1,7 +1,6 @@
 # This file is part of Serendip package. See copyright license in https://github.com/NumericalForge/Serendip.jl
 
-const SR2  = √2.0
-const SR3  = √3.0
-const HALF = 0.5
-const _1_2 = 0.5
-const _1_3 = 1.0/3.0
+const SR2   = √2.0
+const SR3   = √3.0
+const _1_2  = 0.5
+const _1_3  = 1.0/3.0
