51bbc9cc2014878aa0e02943a14fb7c3d6613289
Raul Durand <raul.durand@gmail.com>
2026-02-05 20:22:57 -0300

    refactor: streamline geometry creation and mapping in dowel.jl

--- test/mech/elements/line_interface/dowel.jl
+++ test/mech/elements/line_interface/dowel.jl
@@ -1,65 +1,38 @@
 using Serendip
 
-geo = GeoModel(size=1)
-s = 0.2
+geo = GeoModel(size=0.1)
 
-p1 = addpoint!(geo, 0.0, 0.0, size=s)
-p2 = addpoint!(geo, 1.0, 0.0, size=s)
-p3 = addpoint!(geo, 1.0, 1.0, size=s)
-p4 = addpoint!(geo, 0.0, 1.0, size=s)
+# Bulk
+add_rectangle(geo, [0.0, 0.0, 0.0], 1.0, 1.0, tag="bulks")
 
-addpath!(geo, :M, p1, :L, p2, :L, p3, :L, p4, :L, p1)
+# Bar
+p1 = add_point(geo, [0.2, 0.2, 0.0])
+p2 = add_point(geo, [0.5, 0.5, 0.0])
+p3 = add_point(geo, [0.6, 0.5, 0.0])
+p4 = add_point(geo, [0.9, 0.5, 0.0])
+p5 = add_point(geo, [1.2, 0.5, 0.0])
 
-pb1 = Point(0.2, 0.2)
-pb2 = Point(0.5, 0.5)
-pb3 = Point(0.6, 0.5)
-pb4 = Point(0.9, 0.5)
-pb5 = Point(1.2, 0.5)
-bar = addsubpath!(geo, :M, pb1, :C, pb2, pb3, pb4, :L, pb5)
-array!(geo, bar, ny=2, dy=0.2)
+c1 = add_bezier(geo, [p1, p2, p3, p4])
+l1 = add_line(geo, p4, p5)
+add_path(geo, [c1, l1], tag="lines", interface_tag="linejoints")
 
 mesh = Mesh(geo)
-
 save(mesh, "dowel.vtu")
 
 # FEM
-mats = [
-        :bulks => MechBulk => LinearElastic => (E=24e2, nu=0.2),
+mapper = RegionMapper()
+add_mapping(mapper, :bulk, MechBulk, LinearElastic, E=24e2, nu=0.2)
+add_mapping(mapper, "lines", MechBeam, LinearElastic, E=200e6, A=0.00011)
+add_mapping(mapper, "linejoints", MechBondSlip, LinearBondSlip, kn=5000, ks=6000, p=0.25)
 
-        # :lines => MechBar => LinearElastic => (E=200e6, A=0.00011),
-        :lines => MechBeam => LinearElastic => (E=200e6, A=0.00011),
-
-        :linejoints => MechBondSlip => LinearBondSlip => (kn=5000, ks=6000, p=0.25),
-        # :linejoints => MechBondSlip => CebBondSlip => (taumax=12, taures=3, s1=0.001, s2=0.0011, s3=0.004, alpha=0.5, beta=0.5, ks=(12/0.001)*5, kn=5000, p=0.25)
-       ]
-
-model = FEModel(mesh, mats, stress_state=:plane_stress, ctx, thickness=0.1)
-tag!(model.elems.lines.nodes[x>=1], "outside")
+model = FEModel(mesh, mapper, stress_state=:plane_stress, thickness=0.1)
 
 ana = MechAnalysis(model)
-bcs = [
-    y==0 => NodeBC(ux=0, uy=0),
-    # "outside" => NodeBC(uy=0, ux=0.01),
-    (x==1.2, y==0.5) => NodeBC(ux=0.01, uy=-0.01),
-]
 
-addstage!(ana, bcs, nincs=10, nouts=2)
-solve!(ana, tol=0.01, maxits=3, autoincr=true)
+stage = add_stage(ana, nincs=10, nouts=2)
+add_bc(stage, :node, y==0, ux=0, uy=0)
+add_bc(stage, :node, (x==1.2, y==0.5), ux=0.01, uy=-0.01)
+
+run(ana, tol=0.01, maxits=3, autoinc=true)
 
 save(model, "dowel.vtu")
-
-# ana = MechAnalysis()
-# model = FEModel(mesh, mats, ana)
-
-# tag!(model.elems.lines.nodes[x>=1], "outside")
-
-# bcs = [
-#     y==0 => NodeBC(ux=0, uy=0),
-#     # "outside" => NodeBC(uy=0, ux=0.01),
-#     and(x==1.2, y==0.5) => NodeBC(ux=0.01, uy=-0.01),
-# ]
-
-# addstage!(model, bcs, nincs=10, nouts=2)
-# solve!(model, tol=0.01, maxits=3, autoincr=true)
-
-# save(model, "dowel.vtu")
