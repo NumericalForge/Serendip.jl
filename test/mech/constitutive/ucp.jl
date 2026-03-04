92ddf2aa27dfa05e22f664672d11d8f45a17e2ae
Raul Durand <raul.durand@gmail.com>
2026-03-04 20:33:45 -0300

    update compression and tension tests

--- test/mech/constitutive/ucp.jl
+++ test/mech/constitutive/ucp.jl
@@ -7,7 +7,7 @@ h = 0.2
 
 geo = GeoModel()
 
-add_block(geo, [0,0,0], ℓ,ℓ,h, shape=HEX8, nx=1, ny=1, nz=1)
+add_block(geo, [0,0,0], ℓ,ℓ,h, shape=HEX8, nx=2, ny=2, nz=2)
 
 
 mesh= Mesh(geo, quadratic=true)
@@ -25,30 +25,34 @@ add_mapping(mapper, "solids", MechBulk, UCP,
 
 # ❱❱❱ Analysis 1: compression
 
-# model = FEModel(mesh, mapper)
-# ana = MechAnalysis(model, outkey="compression")
+# printstyled("\nCompression test\n", color=:yellow, bold=true)
 
-# log = add_logger(ana, :ip, [0.0, 0.0, 0.0])
-# add_monitor(ana, :ip, [0.0, 0.0, 0.0], :σzz)
+model = FEModel(mesh, mapper)
+ana = MechAnalysis(model, outkey="compression")
 
-# stage1 = add_stage(ana)
-# uu = -0.002
-# add_bc(stage1, :node, x==0, ux=0)
-# add_bc(stage1, :node, y==0, uy=0)
-# add_bc(stage1, :node, z==0, uz=0)
-# # add_bc(stage1, :node, x==ℓ, ux=uu)
-# # add_bc(stage1, :node, y==ℓ, uy=uu)
-# add_bc(stage1, :node, z==h, uz=uu)
+log_c = add_logger(ana, :ip, [ℓ/2, ℓ/2, h/2], "ip-compression.dat")
+add_monitor(ana, :ip, [ℓ/2, ℓ/2, h/2], :σzz)
 
-# run(ana, autoinc=true, tol=0.006, rspan=0.02, dT0=0.002,)
+stage1 = add_stage(ana)
+uu = -0.0015
+add_bc(stage1, :node, x==0, ux=0)
+add_bc(stage1, :node, y==0, uy=0)
+add_bc(stage1, :node, z==0, uz=0)
+# add_bc(stage1, :node, x==ℓ, ux=uu)
+# add_bc(stage1, :node, y==ℓ, uy=uu)
+add_bc(stage1, :node, z==h, uz=uu)
+
+run(ana, autoinc=true, tol=0.1, rspan=0.02, dT0=0.002, quiet=false)
 
 # ❱❱❱ Analysis 2: tension
+
+printstyled("\nTension test\n", color=:yellow, bold=true)
 
 model = FEModel(mesh, mapper)
 ana = MechAnalysis(model, outkey="tension")
 
-log = add_logger(ana, :ip, [ℓ/2, ℓ/2, h/2], "ip.dat")
-add_monitor(ana, :ip, [ℓ/2, ℓ/2, h/2], :σxx)
+log_t = add_logger(ana, :ip, [ℓ/2, ℓ/2, h/2], "ip-tension.dat")
+add_monitor(ana, :ip, [ℓ/2, ℓ/2, h/2], :σzz)
 
 stage1 = add_stage(ana, nouts=10)
 uu = 0.0002
@@ -58,24 +62,26 @@ add_bc(stage1, :node, z==0, uz=0)
 # add_bc(stage1, :node, x==ℓ, ux=uu)
 add_bc(stage1, :node, z==h, uz=uu)
 
-run(ana, autoinc=true, tol=0.5, quiet=false)
+run(ana, autoinc=true, tol=0.1, quiet=false)
 
 
 # ❱❱❱ Post-process
 
-table = log.table
+table_c = log_c.table
+table_t = log_t.table
 
 chart = Chart(
     xlabel=t"$ε_(z z)$",
     ylabel=t"$σ_(z z)$",
 )
-add_series(chart, table["εzz"], table["σzz"], mark=:circle)
+add_line(chart, table_c["εzz"], table_c["σzz"], mark=:circle)
+add_line(chart, table_t["εzz"], table_t["σzz"], mark=:utriangle, mark_size=1)
 save(chart, "σ-ε.pdf")
 
 chart = Chart(
     xlabel=t"$ξ$",
     ylabel=t"$ρ$",
 )
-add_series(chart, table["ξ"], table["ρ"], mark=:circle)
+add_line(chart, table_c["ξ"], table_c["ρ"], mark=:circle)
+add_line(chart, table_t["ξ"], table_t["ρ"], mark=:utriangle)
 save(chart, "ξ-ρ.pdf")
-
