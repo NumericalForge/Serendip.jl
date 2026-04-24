709883126a0bd3b9cfc99b0832497cf8b70b0595
Raul Durand <raul.durand@gmail.com>
2026-04-24 20:47:51 -0300

    contact and cohesive tests updated

--- test/mech/constitutive/cohesive.jl
+++ test/mech/constitutive/cohesive.jl
@@ -4,9 +4,10 @@ using Test
 # ❱❱❱ Geometry and mesh
 
 geo = GeoModel()
-bl1  = add_block(geo, [0, 0], 0.1, 0.1, 0.0; nx=1, ny=1, shape=:quad4, tag="bulk")
-bl2  = add_block(geo, [0.1, 0], 0.1, 0.1, 0.0; nx=1, ny=1, shape=:quad4, tag="bulk")
+bl1  = add_block(geo, [0, 0], 0.1, 0.1, 0.0; nx=1, ny=1, shape=:quad4, tag="solid")
+bl2  = add_block(geo, [0.1, 0], 0.1, 0.1, 0.0; nx=1, ny=1, shape=:quad4, tag="solid")
 mesh = Mesh(geo)
+
 add_cohesive_elements(mesh, tag="interface")
 
 left_elem = select(mesh, :element, :bulk, x<=0.1)
@@ -17,8 +18,8 @@ select(get_nodes(right_elem), tag="right")
 
 E    = 27.e6
 nu   = 0.2
-ft   = 2.4e3
 fc   = -24e3
+ft   = 2.4e3
 wc   = 1.7e-4
 zeta = 5.0
 
@@ -38,7 +39,6 @@ models = (
     (cmodel=AsinhYieldCohesive, props=(E=E, nu=nu, fc=fc, ft=ft, zeta=zeta, wc=wc, alpha=0.33, beta=0.2, theta=1.0, psi=1.4), mark=:circle),
 )
 
-
 grid = ChartGrid(
     # size=(16cm, 6cm), 
     size=(16cm, 24cm),
@@ -48,7 +48,6 @@ grid = ChartGrid(
     quiet=true,
 )
 
-
 for (i,trajectory) in enumerate(trajectories)
     printstyled("\nTrajectory: $(trajectory)\n\n", color=:cyan, bold=true)
     
@@ -56,26 +55,24 @@ for (i,trajectory) in enumerate(trajectories)
     chart2 = Chart(legend=:top_right, xlabel="`w`", ylabel="`σ_n`")
 
     for model in models
-        
         cmodel = model.cmodel
-        props = model.props
-        mark = model.mark
+        props  = model.props
+        mark   = model.mark
 
         printstyled("\n$(string(cmodel)):\n\n", color=:yellow, bold=true)
 
         mapper = RegionMapper()
-        add_mapping(mapper, "bulk", MechSolid, LinearElastic, E=E, nu=nu)
+        add_mapping(mapper, "solid", MechSolid, LinearElastic, E=E, nu=nu)
         add_mapping(mapper, "interface", MechCohesive, cmodel; props...)
         
         model = FEModel(mesh, mapper, stress_state=:plane_stress, thickness=1.0)
         ana = MechAnalysis(model)
         
-        joint = select(model.elems, "interface")
-        select( get_ips(joint), tag="jips" )
+        select(model, :element, "interface", :ip, tag="jips" )
         log1 = add_logger(ana, :ip, "jips", string(cmodel)*".dat")
         add_monitor(ana, :ip, "jips", (:σn, :τ))
         
-        stage = add_stage(ana, nincs=80, nouts=80)
+        stage = add_stage(ana, nincs=80, nouts=20)
 
         add_bc(stage, :node, "left", ux=0, uy=0)
 
@@ -89,7 +86,7 @@ for (i,trajectory) in enumerate(trajectories)
             add_bc(stage, :node, "right", ux=-0.000001, uy=0.0002)
         end
 
-        status = run(ana, autoinc=true, maxits=10, tol=0.1, rspan=0.03, dTmax=0.1, quiet=false)
+        status = run(ana, autoinc=true, tol=0.1, rspan=0.03, dTmax=0.1, quiet=false)
         
         add_line(chart1, log1.table["σn"], log1.table["τ"], label=string(cmodel), mark=mark)
         add_line(chart2, log1.table["w"], log1.table["σn"], label=string(cmodel), mark=mark)
--- test/mech/constitutive/contact.jl
+++ test/mech/constitutive/contact.jl
@@ -4,61 +4,102 @@ using Test
 # ❱❱❱ Geometry and mesh
 
 geo = GeoModel()
-bl1  = add_block(geo, [0, 0], 0.1, 0.1, 0.0; nx=1, ny=1, shape=:quad4, tag="bulk")
-bl2  = add_block(geo, [0.1, 0], 0.1, 0.1, 0.0; nx=1, ny=1, shape=:quad4, tag="bulk")
+bl1  = add_block(geo, [0, 0], 0.1, 0.1, 0.0; nx=1, ny=1, shape=:quad4, tag="left")
+bl2  = add_block(geo, [0.1, 0], 0.1, 0.1, 0.0; nx=1, ny=1, shape=:quad4, tag="right")
 mesh = Mesh(geo)
-select(mesh, :element, x<=0.1, tag="left")
 
 add_contact_elements(mesh, tag="interface")
+
+select(mesh, :element, "left", :node, tag="left")
+select(mesh, :element, "right", :node, tag="right")
+
+# left_elem = select(mesh, :element, :bulk, x<=0.1)
+# select(get_nodes(left_elem), tag="left")
+
+# right_elem = select(mesh, :element, :bulk, x>=0.1)
+# select(get_nodes(right_elem), tag="right")
 
 E    = 27.e6
 nu   = 0.2
 ft   = 2.4e3
-fc   = -24e3
-zeta = 5.0
 wc   = 1.7e-4
 kn   = 2e10
 ks   = 1.8e7
+mu   = 1.5
+zeta = 5.0
 
 # ❱❱❱ Finite element analyses
 
-models_props_d = Dict(
-    MohrCoulombContact => (ks=ks, kn=kn, ft=ft, wc=wc, mu=0.9)
+trajectories = [ 
+    "pure extension",
+    "extension with shear",
+    "pure shear",
+    "compression with shear" 
+]
+
+models = (
+    (cmodel=MohrCoulombContact, props=(ks=ks, kn=kn, ft=ft, wc=wc, mu=1.4), mark=:triangle),
+    (cmodel=CoulombContact, props=(kn=kn, ks=ks, mu=mu), mark=:circle),
 )
 
-model_elem_d = Dict(
-    MohrCoulombContact => MechContact
+grid = ChartGrid(
+    # size=(16cm, 6cm), 
+    size=(16cm, 24cm),
+    background=:old_paper, 
+    row_headers=trajectories,
+    column_headers=[ "`σ_n` vs `τ`", "`w` vs `σ_n`" ],
+    quiet=true,
 )
 
-# ❱❱❱ Finite element analysis using LinearCohesive
-chart = Chart(legend=:top_left, xlabel="σn", ylabel="τ")
+for (i,trajectory) in enumerate(trajectories)
+    printstyled("\n\nTrajectory: $(trajectory)\n\n", color=:yellow, bold=true)
+    
+    chart1 = Chart(legend=:top_left, xlabel="`σ_n`", ylabel="`τ`")
+    chart2 = Chart(legend=:top_right, xlabel="`w`", ylabel="`σ_n`")
 
-for (cmodel, props) in models_props_d
-    printstyled("\n$(string(cmodel)):\n\n", color=:yellow, bold=true)
-    elem_type = model_elem_d[cmodel]
-    
-    mapper = RegionMapper()
-    add_mapping(mapper, :bulk, MechSolid, LinearElastic, E=E, nu=nu)
-    add_mapping(mapper, "interface", elem_type, cmodel; props...)
-    
-    model = FEModel(mesh, mapper, stress_state=:plane_stress, thickness=1.0)
-    ana = MechAnalysis(model)
-    
-    contact = select(model.elems, "interface")
-    select( get_ips(contact), tag="jips" )
-    log1 = add_logger(ana, :ip, "jips")
-    add_monitor(ana, :ip, "jips", (:σn, :τ))
-    
-    stage = add_stage(ana, nincs=80, nouts=20)
-    add_bc(stage, :node, (x==0,y==0), uy=0)
-    add_bc(stage, :node, x==0, ux=0)
-    add_bc(stage, :node, x==0.2, ux=0.0001)
-    
-    status = run(ana, autoinc=true, tol=0.05, rspan=0.02, dTmax=0.03, quiet=false)
-    add_series(chart, log1.table["σn"], log1.table["τ"], label=string(cmodel), mark=:circle)
+    for model in models
+        cmodel = model.cmodel
+        props  = model.props
+        mark   = model.mark
 
-    # break
-    @test status.successful
+        printstyled("\n$(string(cmodel))\n\n", color=:yellow, bold=true)
+
+        mapper = RegionMapper()
+        add_mapping(mapper, :solid, MechSolid, LinearElastic, E=E, nu=nu)
+        add_mapping(mapper, "interface", MechContact, cmodel; props...)
+        
+        model = FEModel(mesh, mapper, stress_state=:plane_stress, thickness=1.0)
+        ana = MechAnalysis(model)
+        # global mm = model
+        
+        select(model, :element, "interface", :ip, tag="c-ips" )
+        log1 = add_logger(ana, :ip, "c-ips", string(cmodel)*".dat")
+        add_monitor(ana, :ip, "c-ips", (:σn, :τ))
+        
+        stage = add_stage(ana, nincs=80, nouts=20)
+
+        add_bc(stage, :node, "left", ux=0, uy=0)
+
+        if trajectory == "pure extension"
+            add_bc(stage, :node, "right", ux=0.0002)
+        elseif trajectory == "extension with shear"
+            add_bc(stage, :node, "right", ux=0.00001, uy=0.0001)
+        elseif trajectory == "pure shear"
+            add_bc(stage, :node, "right", ux=0.0, uy=0.001)
+        elseif trajectory == "compression with shear"
+            add_bc(stage, :node, "right", ux=-0.000001, uy=0.0002)
+        end
+
+        status = run(ana, autoinc=true, tol=0.1, rspan=0.03, dTmax=0.1, quiet=false)
+        # @show log1.table
+        
+        add_line(chart1, log1.table["σn"], log1.table["τ"], label=string(cmodel), mark=mark)
+        add_line(chart2, log1.table["w"], log1.table["σn"], label=string(cmodel), mark=mark)
+
+    end
+    add_chart(grid, chart1, (i, 1))
+    add_chart(grid, chart2, (i, 2))
+
 end
 
-save(chart, "contact-models-comparison.pdf")
+save(grid, "contact-models.pdf")
